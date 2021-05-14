c41b36c9de7d50afa177b30f456e98684d07087d
anugrahakb <anugrahakb162@gmail.com>
2021-05-14 21:45:47 +0530

    first commit

---
+++ Spring_Security/src/main/java/security_1/HelloController.java
@@ -0,0 +1,17 @@
+package security_1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/admin")
+	public String admin()
+	{	
+		return "Welcome Admin";
+	}
+	
+
+}
---
+++ Spring_Security/src/main/java/security_2/Security2and3Application.java
@@ -0,0 +1,13 @@
+package security_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security2and3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security2and3Application.class, args);
+	}
+
+}
